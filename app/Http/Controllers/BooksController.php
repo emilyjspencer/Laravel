ffe65052194a5d15f3824759579be63bc99ecb29
Emily Spencer <emilyspencer89@hotmail.co.uk>
2020-09-24 00:41:14 +0100

    Creates database. Removes hardcoded books. Data being pulled in from teh database

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -6,27 +6,23 @@ use Illuminate\Http\Request;
 
 class BooksController extends Controller
 {
-    public function show($book)
+    public function show($slug)
 
     {
-    $books = [
-        'book-one' => 'Frankenstein',
-        'book-two' => 'Proust and the Squid',
-        'book-three' => 'Out of Africa',
-        'book-four' => 'Five on Brexit Island',
-        'book-five' => 'Year of Wonder',
-        'book-six' => '#Girlboss'
-    ];
+    
 
-    if(! array_key_exists($book, $books)) {
-        abort(404, 'Sorry, that page was not found');
-    }
+        $book = \DB::table('books')->where('slug', $slug)->first();
+
+
+        
+
+    
  
 
     
 
     return view('book', [
-        'book' => $books[$book]
+        'book' => $book
     ]);
 
     }
--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -5,7 +5,7 @@
 </head>
 <body>
   <h2 align="center">Book List</h2>
-  <p align="center">{{ $book }}</p>
+  <p align="center">{{ $book->title }}</p>
 
 
 </body>
