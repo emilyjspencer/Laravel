13a23b3671a58caeb51f70fd02558d55b751e62c
Emily Spencer <emilyspencer89@hotmail.co.uk>
2020-09-28 19:02:51 +0100

    Adds welcome test view

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+  <html>
+      <head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+
+          <title>Test</title>
+
+          <link href="https://fonts.google.apis.com/css?family=Nunito:200,600 rel="stylesheet">
+
+          <style>
+              html, body {
+                  background-color: pink;
+                  color: black;
+                  font-family: 'Nunito', sans-serif;
+                  font-weight: 200;
+                  height: 100vh;
+                  margin: 0;
+              }
+
+              .flex-center {
+                  align-items: center;
+                  display: flex;
+                  justify-conent: center;
+              }
+
+              position-ref {
+                  position: relative;
+              }
+
+              .top-right {
+                  position: absolute;
+                  right: 10px;
+                  top: 18px;
+              }
+
+              .content {
+                  text-align: center;
+              }
+
+              .title {
+                  font-size: 84px;
+              }
+
+              .links > a {
+                  color: black;
+                  font-size: 0 25px;
+                  font-weight: 600;
+                  padding: 0 25px;
+                  letter-spacing: .1rem;
+                  text-decoration: none;
+                  text-transform: uppercase;
+              }
+
+            </style>
+
+            <body>
+              <div class="content">
+                <div class="title m-b-md">
+                  Test
+                </div>
+
+                <div class="links">
+                  <a href="/">1</a>
+                  <a href="/">2</a>
+                  <a href="/">3</a>
+                  <a href="/">4</a>
+                  <a href="/">5</a>
+                  <a href="/">6</a>
+                </div>
+
+                </div>
+            </body>
+</html>
